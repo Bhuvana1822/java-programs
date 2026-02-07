5871a3a70fa48d566d4310466040a65a5004dd7e
Bhuvana1822 <bhuvanareddy2211@gmail.com>
2026-02-07 14:37:20 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,26 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=18;
+        int b=22;
+        System.out.println(a==b);
+        System.err.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+        System.out.println(a++);
+        System.out.println(a++);
+        System.out.println(--a);
+        System.out.println(--a);
+        System.out.println(a&b);
+        System.out.println(a^b);
+        System.out.println(~a);
+        System.out.println(a+=2);
+        System.out.println(a-=2);
+        System.out.println(a*=2);
+        System.out.println(a/=2);
+        System.out.println(a%=2);
+        System.out.println(a>>1);
+        System.out.println(a<<1);
+    }
+}
